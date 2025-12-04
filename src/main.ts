70eb9a9ebba129a431ca0c787ef06d024f51fa0d
rifky <mochamad.rifaldy@iconpln.co.id>
2025-12-04 10:01:51 +0700

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { applicationLogger } from './utils/logger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  applicationLogger.error('Inisialisasi server NestJS dimulai...');
   app.use(cookieParser());
   app.enableCors({
     origin: [],
